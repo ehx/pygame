dbb3329da421e5867d54bd73f6cf5d7c29ba8d63
eloyc <eloycolella@gmail.com>
2016-09-01 18:01:46 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,6 @@
+from test import Mago
+
+eloy = Mago('Eloy', 1, 100)
+franco = Mago('Franco', 1, 50)
+
+print(eloy.getVida())
---
+++ test.py
@@ -0,0 +1,20 @@
+class Mago(object):
+    cantidad = 0
+    nombres = []
+
+    @classmethod
+    def busca(cls, nombre):
+        return cls._level >= level
+
+    def __init__(self, nombre, nivel, vida):
+        self.nombre = nombre
+        self.nivel = nivel
+        self.vida = vida
+        Mago.cantidad += 1
+
+    def poder(self, tipo, danio, victima):
+        Mago.buscar('Franco')
+        return self.balance
+
+    def getVida(self):
+        return self.vida
